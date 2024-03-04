324125abc1d7e3aed6992126e92bdf553e487631
Cristian Serena <devcristian02@gmail.com>
2024-03-03 23:22:52 -0300

    incluido a classe Aluno e deixando o metodo main mais limpo

---
+++ Aluno.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace Notas_alunos
+{
+    public class Aluno
+    {
+        public double Nota1, Nota2, Nota3;
+        public double Resultado;
+        public string NomeAluno;
+        public double PontosFaltantes;
+        public string dot = new string('*', 50);
+        public double CalculaNota(string NomeAluno, double Nota1, double Nota2, double Nota3, double Resultado, double PontosFaltantes )
+        {
+            Resultado = (Nota1 * 30 / 100) + (Nota2 * 35 / 100) + (Nota3 * 35 / 100);
+
+            if (Resultado >= 60)
+            {
+                Console.WriteLine($"O Aluno {NomeAluno} está APROVADO com o resultado {Resultado.ToString("F2")}");
+            }
+            else
+            {
+                PontosFaltantes = 60 - Resultado;
+                Console.WriteLine($"O aluno {NomeAluno} está REPROVADO com nota final {Resultado.ToString("F2")}.");
+                Console.WriteLine($"Faltam {PontosFaltantes.ToString("F2")} pontos para o aluno ser aprovado.");
+                Console.WriteLine(dot);
+            }
+
+            return Resultado;
+        }
+
+    }
+}
--- NotasAlunos.cs
+++ NotasAlunos.cs
@@ -6,43 +6,28 @@ namespace Notas_alunos
     {
         static void Main(string[] args)
         {
-            string dot = new string('*', 50);
-            double nota1, nota2, nota3;
-            double resultado;
-            string nomeAluno;
-            double pontosFaltantes;
+            string dot = new string('.', 50);
+
+            Aluno aluno = new Aluno();
             
             Console.WriteLine("Sistema Notas de Alunos");
             Console.WriteLine(dot);
     
             Console.Write("Digite o Nome do Aluno: ");
-            nomeAluno = Console.ReadLine();
+            aluno.NomeAluno = Console.ReadLine();
 
             Console.Write("Digite a Nota do primeiro trimestre: ");
-            nota1 = double.Parse(Console.ReadLine());
+            aluno.Nota1 = double.Parse(Console.ReadLine());
 
             Console.Write("Digite a Nota do segundo trimestre: ");
-            nota2 = double.Parse(Console.ReadLine());
+            aluno.Nota2 = double.Parse(Console.ReadLine());
 
             Console.Write("Digite a Nota do terceiro trimestre: ");
-            nota3 = double.Parse(Console.ReadLine());
+            aluno.Nota3 = double.Parse(Console.ReadLine());
             Console.WriteLine(dot);
 
-            // Calcula as notas do aluno
+            aluno.CalculaNota(aluno.NomeAluno, aluno.Nota1, aluno.Nota2, aluno.Nota3, aluno.Resultado, aluno.PontosFaltantes);
 
-            resultado = (nota1 * 30 / 100) + (nota2 * 35 / 100) + (nota3 * 35 / 100);
-
-            if(resultado >= 60)
-            {
-                Console.WriteLine($"O Aluno {nomeAluno} está APROVADO com o resultado {resultado.ToString("F2")}");
-            }
-            else
-            {
-                pontosFaltantes = 60 - resultado;
-                Console.WriteLine($"O aluno {nomeAluno} está REPROVADO com nota final {resultado.ToString("F2")}.");
-                Console.WriteLine($"Faltam {pontosFaltantes.ToString("F2")} pontos para o aluno ser aprovado.");
-                Console.WriteLine(dot);
-            }
         }
     }
 }
